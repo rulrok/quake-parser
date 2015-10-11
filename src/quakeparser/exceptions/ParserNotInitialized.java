a0cd7833f32d7b59eaf68d179f632c65fdb2aead
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-10-11 02:56:52 +0000

    Exception class for when parser is not first initiated

---
+++ src/quakeparser/exceptions/ParserNotInitialized.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quakeparser.exceptions;
+
+/**
+ *
+ * @author rulrok
+ */
+public class ParserNotInitialized extends Exception {
+
+    public ParserNotInitialized(String message) {
+        super(message);
+    }
+}
