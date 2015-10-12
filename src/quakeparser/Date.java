ab39aa30a194035b16afa36d01b02a30091f3356
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-10-12 13:21:45 +0000

    Added empty constructor

--- src/quakeparser/Date.java
+++ src/quakeparser/Date.java
@@ -16,6 +16,10 @@ public class Date implements IDate {
     private final int hours;
     private final int minutes;
 
+    public Date() {
+        this(0, 0);
+    }
+
     public Date(int hours, int minutes) {
         this.hours = hours % 24;
         this.minutes = minutes % 60;
