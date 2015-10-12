ccd4fb5dd56652ffac0e254d92e3898f03477388
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-10-12 02:02:49 +0000

    Added new interface

---
+++ src/quakeparser/contracts/IDate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quakeparser.contracts;
+
+/**
+ *
+ * @author rulrok
+ */
+public interface IDate extends Comparable<IDate> {
+
+    public int hours();
+
+    public int minutes();
+
+    public boolean after(IDate date);
+
+    public boolean before(IDate date);
+
+}
