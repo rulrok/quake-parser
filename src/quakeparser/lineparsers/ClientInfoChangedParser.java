29d638763f3e5827bbba703344dee95312d54d96
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-10-12 16:46:53 +0000

    Implemented client info changed line parser to conform with test

--- src/quakeparser/lineparsers/ClientInfoChangedParser.java
+++ src/quakeparser/lineparsers/ClientInfoChangedParser.java
@@ -18,7 +18,29 @@ public class ClientInfoChangedParser extends AbstractLineParser {
     @Override
     public ILine processLine(ILine line) {
         if (line.event().equals(Event.ClientUserinfoChanged)) {
-            return null;
+            String rawLine = line.rawLine();
+
+            String[] split = rawLine.split("\\s+");
+
+            line.setSubject(split[2]);
+
+            String info = split[3];
+
+            String[] details = info.split("\\\\");
+
+            for (int i = 0; i < details.length; i++) {
+                String detail = details[i];
+
+                switch (detail) {
+                    case "n":
+                        line.setIndirectObject(details[i + 1]);
+                        i++;
+                        break;
+                }
+
+            }
+
+            return line;
         } else if (successor != null) {
             return successor.processLine(line);
         } else {
