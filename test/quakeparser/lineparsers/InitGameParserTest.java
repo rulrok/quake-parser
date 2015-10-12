b388cae558a9a45d7f41c927274b13b66336134c
Reuel Ramos Ribeiro <rulrok@gmail.com>
2015-10-12 21:34:39 +0000

    Test for init game line parser

---
+++ test/quakeparser/lineparsers/InitGameParserTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quakeparser.lineparsers;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import quakeparser.Date;
+import quakeparser.Event;
+import quakeparser.LogLine;
+import quakeparser.contracts.ILine;
+
+/**
+ *
+ * @author rulrok
+ */
+public class InitGameParserTest {
+
+    public InitGameParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    String initGameLine = "1:47 InitGame: \\sv_floodProtect\\1\\sv_maxPing\\0\\sv_minPing\\0\\sv_maxRate\\10000\\sv_minRate\\0\\sv_hostname\\Code Miner Server\\g_gametype\\0\\sv_privateClients\\2\\sv_maxclients\\16\\sv_allowDownload\\0\\bot_minplayers\\0\\dmflags\\0\\fraglimit\\20\\timelimit\\15\\g_maxGameClients\\0\\capturelimit\\8\\version\\ioq3 1.36 linux-x86_64 Apr 12 2009\\protocol\\68\\mapname\\q3dm17\\gamename\\baseq3\\g_needpass\\0";
+
+    /**
+     * Test of processLine method, of class InitGameParser.
+     */
+    @Test
+    public void testProcessLine() {
+        System.out.println("exit line parser");
+
+        ILine line = new LogLine(new Date(1, 47), Event.InitGame);
+        line.setRawLine(initGameLine);
+
+        InitGameParser instance = new InitGameParser();
+
+        ILine expResult = new LogLine(new Date(1, 47), Event.InitGame, "\\sv_floodProtect\\1\\sv_maxPing\\0\\sv_minPing\\0\\sv_maxRate\\10000\\sv_minRate\\0\\sv_hostname\\Code Miner Server\\g_gametype\\0\\sv_privateClients\\2\\sv_maxclients\\16\\sv_allowDownload\\0\\bot_minplayers\\0\\dmflags\\0\\fraglimit\\20\\timelimit\\15\\g_maxGameClients\\0\\capturelimit\\8\\version\\ioq3 1.36 linux-x86_64 Apr 12 2009\\protocol\\68\\mapname\\q3dm17\\gamename\\baseq3\\g_needpass\\0", "", "");
+        ILine result = instance.processLine(line);
+
+        assertEquals(expResult, result);
+    }
+
+}
